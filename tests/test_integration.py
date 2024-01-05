6b7137accfb96f7d9d3d2dc2ee4a5c791a3c526b
José M. Ramírez <moscaelectronica@gmail.com>
2024-01-05 19:05:55 +0100

    Refactoring the project

---
+++ tests/test_integration.py
@@ -0,0 +1,9 @@
+def test_example_integration():
+    # Arrange
+    instance = "some task"
+
+    # Act
+    result = "result of task"
+
+    # Assert
+    assert result == "expected_result"
---
+++ tests/test_main_module.py
@@ -0,0 +1,9 @@
+def test_example_unit():
+    # Arrange
+    instance = "some task"
+
+    # Act
+    result = "result of task"
+
+    # Assert
+    assert result == "result of task"
