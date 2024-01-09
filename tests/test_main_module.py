e4a4d027405ad58dac83ebc222a31cd1674d7a8e
José M. Ramírez <moscaelectronica@gmail.com>
2024-01-09 17:06:27 +0100

    A basic testing environment

--- tests/test_main_module.py
+++ tests/test_main_module.py
@@ -1,9 +1,24 @@
-def test_example_unit():
-    # Arrange
-    instance = "some task"
+import pytest
+from src.main_module import Car
 
-    # Act
-    result = "result of task"
 
-    # Assert
-    assert result == "result of task"
+@pytest.fixture
+def my_car():
+    return Car(50)
+
+
+speed_data = {45, 50, 55, 100}
+
+
+@pytest.mark.parametrize("speed_brake", speed_data)
+def test_car_brake(speed_brake):
+    car = Car(50)
+    car.brake()
+    assert car.speed == speed_brake
+
+
+@pytest.mark.parametrize("speed_accelerate", speed_data)
+def test_car_accelerate(speed_accelerate):
+    car = Car(50)
+    car.accelerate()
+    assert car.speed == speed_accelerate
